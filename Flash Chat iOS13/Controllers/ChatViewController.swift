aaf8090236cfa974f11e19b765303648f4f22646
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2025-02-26 16:09:59 +0900

    Finish lesson 187

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -1,25 +1,26 @@
-//
-//  ChatViewController.swift
-//  Flash Chat iOS13
-//
-//  Created by Angela Yu on 21/10/2019.
-//  Copyright © 2019 Angela Yu. All rights reserved.
-//
-
+import FirebaseAuth
+import FirebaseCore
+import FirebaseFirestore
 import UIKit
 
 class ChatViewController: UIViewController {
+    @IBOutlet var tableView: UITableView!
+    @IBOutlet var messageTextfield: UITextField!
 
-    @IBOutlet weak var tableView: UITableView!
-    @IBOutlet weak var messageTextfield: UITextField!
-    
     override func viewDidLoad() {
+        title = "⚡️FlashChat"
         super.viewDidLoad()
-
+        navigationItem.hidesBackButton = true
     }
-    
-    @IBAction func sendPressed(_ sender: UIButton) {
-    }
-    
 
+    @IBAction func sendPressed(_ sender: UIButton) {}
+
+    @IBAction func logoutPressed(_ sender: UIBarButtonItem) {
+        do {
+            try Auth.auth().signOut()
+            navigationController?.popToRootViewController(animated: true)
+        } catch let signOutError as NSError {
+            print("Error signing out: %@", signOutError)
+        }
+    }
 }
--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -9,14 +9,18 @@
 import UIKit
 
 class WelcomeViewController: UIViewController {
+    @IBOutlet var titleLabel: UILabel!
 
-    @IBOutlet weak var titleLabel: UILabel!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       
+        titleLabel.text = ""
+        var charIndex = 0.0
+        let titleText = "⚡️FlashChat"
+        for letter in titleText {
+            Timer.scheduledTimer(withTimeInterval: 0.1, repeats: false) {
+                _ in self.titleLabel.text?.append(letter)
+            }
+            charIndex += 1
+        }
     }
-    
-
 }
