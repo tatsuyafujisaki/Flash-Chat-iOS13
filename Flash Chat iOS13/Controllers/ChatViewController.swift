0a1379db57fa7176b30dcc18886e015a40301956
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2025-02-27 16:37:56 +0900

    Finish lesson 189

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -7,7 +7,14 @@ class ChatViewController: UIViewController {
     @IBOutlet var tableView: UITableView!
     @IBOutlet var messageTextfield: UITextField!
 
+    var messages: [Message] = [
+        Message(sender: "1@2.com", body: "Hey!"),
+        Message(sender: "a@b.com", body: "Hello!"),
+        Message(sender: "1@2.com", body: "What's up?")
+    ]
+
     override func viewDidLoad() {
+        tableView.dataSource = self
         title = K.appName
         super.viewDidLoad()
         navigationItem.hidesBackButton = true
@@ -22,5 +29,17 @@ class ChatViewController: UIViewController {
         } catch let signOutError as NSError {
             print("Error signing out: %@", signOutError)
         }
+    }
+}
+
+extension ChatViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        messages.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath)
+        cell.textLabel?.text = "\(messages[indexPath.row].body)"
+        return cell
     }
 }
